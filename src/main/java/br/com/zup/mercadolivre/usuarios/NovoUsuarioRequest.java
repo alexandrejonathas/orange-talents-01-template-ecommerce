3b898dcf0ee63f02ad5585362e8e8d3f774f1d92
Jonathas Lima <alexandrejonathas@gmail.com>
2021-02-01 19:52:03 -0300

    Comitando cadastro de usuário com seu teste de integração

---
+++ src/main/java/br/com/zup/mercadolivre/usuarios/NovoUsuarioRequest.java
@@ -0,0 +1,40 @@
+package br.com.zup.mercadolivre.usuarios;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class NovoUsuarioRequest {
+
+	@Email
+	@NotBlank
+	private String login;
+
+	@Size(min = 6)
+	@NotBlank
+	private String senha;
+
+	public NovoUsuarioRequest(@NotEmpty @Email String login, @NotBlank @Size(min = 6) String senha) {
+		this.login = login;
+		this.senha = senha;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [login="+login+", senha="+senha+"]";
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public Usuario toModel() {
+		return new Usuario(login, senha);
+	}
+	
+}
---
+++ src/test/java/br/com/zup/mercadolivre/usuarios/CadastroUsuarioIT.java
@@ -0,0 +1,82 @@
+package br.com.zup.mercadolivre.usuarios;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class CadastroUsuarioIT {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private ObjectMapper objectMapper;	
+	
+	@Test
+	public void deveCadastrarUmUsuario() throws Exception {
+		NovoUsuarioRequest request = new NovoUsuarioRequest("user@email.com", "123456");
+		this.mockMvc.perform(
+				post("/usuarios")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request))
+			)
+			.andDo(print())
+			.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void naoDeveCadastrarUmUsuarioSemLoginESenha() throws Exception {
+		NovoUsuarioRequest request = new NovoUsuarioRequest(null, null);
+		this.mockMvc.perform(
+				post("/usuarios")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request))
+			)
+			.andDo(print())
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.fieldErrors").isArray())
+			.andExpect(jsonPath("$.fieldErrors[?(@.field == 'login')].message").value("must not be blank"))
+			.andExpect(jsonPath("$.fieldErrors[?(@.field == 'senha')].message").value("must not be blank"));
+	}
+	
+	@Test
+	public void naoDeveCadastrarUmUsuarioComEmailInvalido() throws Exception {
+		NovoUsuarioRequest request = new NovoUsuarioRequest("user.com", "123456");
+		this.mockMvc.perform(
+				post("/usuarios")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request))
+			)
+			.andDo(print())
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.fieldErrors").isArray())
+			.andExpect(jsonPath("$.fieldErrors[?(@.field == 'login')].message").value("must be a well-formed email address"));		
+	}
+	
+	@Test
+	public void naoDeveCadastrarUmUsuarioComSenhaMenorQueSeisCaracteres() throws Exception {
+		NovoUsuarioRequest request = new NovoUsuarioRequest("user@email.com", "123");
+		this.mockMvc.perform(
+				post("/usuarios")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request))
+			)
+			.andDo(print())
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.fieldErrors").isArray())
+			.andExpect(jsonPath("$.fieldErrors[?(@.field == 'senha')].message").value("size must be between 6 and 2147483647"));		
+	}	
+	
+}
